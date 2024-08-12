3c910f93cd573621849cb4e46e867f1786f9d343
Moises Merza <moisesmerza@gmail.com>
2024-08-11 22:43:04 -0400

    odoo17

--- hospital_management_system/__manifest__.py
+++ hospital_management_system/__manifest__.py
@@ -2,5 +2,11 @@
     "name":"Sistema de Gestion Hospitalaria",
     "author":"Merzamac",
     "license":"LGPL-3",
-    "version":"17.0.1.0"
+    "version":"17.0.1.0",
+    "data":[
+        "security/ir.model.access.csv",
+        "views/patient_views.xml",
+        "views/menu.xml",
+
+]
 }
--- hospital_management_system/models/Patient.py
+++ hospital_management_system/models/Patient.py
@@ -1,8 +1,8 @@
 from odoo import api, fields, models
-
 class Patient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient Master'
 
-    name = fields.Char(string="name",required=True)
-    date_of_birth = fields.Date(string="Merza")
+    name = fields.Char(string="name", required=True)
+    date_of_birth = fields.Date(string="merzamac")
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
