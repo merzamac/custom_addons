19c36ba2548aab51a7d67988246b1dc26aebff84
Moises Merza <moisesmerza@gmail.com>
2024-08-18 21:42:01 -0400

    update model

--- hospital_management_system/__manifest__.py
+++ hospital_management_system/__manifest__.py
@@ -1,12 +1,13 @@
 {
-    "name":"Sistema de Gestion Hospitalaria",
-    "author":"Merzamac",
-    "license":"LGPL-3",
-    "version":"17.0.1.0",
-    "data":[
-        "security/ir.model.access.csv",
-        "views/patient_views.xml",
-        "views/menu.xml",
+    'name': 'Sistema de Gestion Hospitalaria',
+    'author': 'Merzamac',
+    'license': 'LGPL-3',
+    'version': '17.0.1.0',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/patient_views.xml',
+        'views/menu.xml',
 
-]
+
+    ]
 }
--- hospital_management_system/models/Patient.py
+++ hospital_management_system/models/Patient.py
@@ -1,8 +1,10 @@
 from odoo import api, fields, models
+
+
 class Patient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient Master'
 
-    name = fields.Char(string="name", required=True)
-    date_of_birth = fields.Date(string="merzamac")
+    name = fields.Char(string="Name", required=True)
+    date_of_birth = fields.Date(string="Birth")
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
